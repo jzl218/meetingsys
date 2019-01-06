c0473de340ef6249f1380c8785dd4e95a53131d2
handsomemu <751212612@qq.com>
2019-01-06 21:43:33 +0800

    .

--- src/main/java/api/repository/MeetingAccountRepository.java
+++ src/main/java/api/repository/MeetingAccountRepository.java
@@ -3,9 +3,10 @@ package api.repository;
 import api.Entity.MeetingAcoount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-
+@Transactional
 @Repository
 public interface MeetingAccountRepository extends JpaRepository<MeetingAcoount,Integer> {
     List<MeetingAcoount> findByAccount(String account);
